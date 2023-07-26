3ac24862076ecbeb1fd2c5c5d3797af89727f7ed
Dinesh Rathnasiri <131970897+Dinesh0017@users.noreply.github.com>
2023-07-26 20:53:04 +0530

    Add files via upload

---
+++ Lab 11 2023.07.20/Q1/J230720.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.j230720;
+
+/**
+ *
+ * @author dines
+ */
+public class J230720 {
+
+    public static void main(String[] args) {
+        change_bg_color c= new change_bg_color();
+        c.show();
+    }
+}
